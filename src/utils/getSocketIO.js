aa8f3452ef755a78e20feda18b5e52f7d5b68953
TrongPhan1011 <huutrong2k1@gmail.com>
2022-11-30 21:38:41 +0700

    fix socket

--- src/utils/getSocketIO.js
+++ src/utils/getSocketIO.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_SOCKET, { transports: ['websocket', 'polling', 'flashsocket'] });
+const socket = io('https://n14-lcn-socket.herokuapp.com/', { transports: ['websocket', 'polling', 'flashsocket'] });
 
 export default socket;
